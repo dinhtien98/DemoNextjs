520bec9e44adbd17077b12b00a4279fcb589aa6f
dinhtien98 <ngodinhtien98@gmail.com>
2024-12-31 09:43:57 +0700

    update menu in user

--- src/app/user/tableUser.tsx
+++ src/app/user/tableUser.tsx
@@ -9,8 +9,8 @@ import { DataTableSelectionMultipleChangeEvent, DataTableSelectAllChangeEvent } 
 import { getUser } from '../components/fetchApi';
 import { authOptions } from '../api/auth/[...nextauth]/route';
 import { Button } from 'primereact/button';
-import 'primereact/resources/themes/saga-blue/theme.css'; 
-import 'primereact/resources/primereact.min.css';  
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 
 interface TableUser {
@@ -93,15 +93,26 @@ export default function tableUser({ session: initialSession }: TableUser) {
     return (
         <div className='p-4 m-4 flex flex-col md:flex-row gap-4'>
             <div className='p-4 w-full md:w-1/6 bg-white shadow-lg rounded-lg'>
+                <div>
+                    <a href="/user" className="text-black hover:text-blue-700">
+                        <i className="pi pi-home mr-2"></i>Dashboard
+                    </a>
+                </div>
                 <ul className="menu p-4 text-lg font-bold">
                     <li className="menu-item p-2">
-                        <a href="/user" className="text-black hover:text-blue-700">User</a>
+                        <a href="/user" className="text-black hover:text-blue-700">
+                            <i className="pi pi-user mr-2"></i>User
+                        </a>
                     </li>
                     <li className="menu-item p-2">
-                        <a href="/page" className="text-black hover:text-blue-700">Page</a>
+                        <a href="/page" className="text-black hover:text-blue-700">
+                            <i className="pi pi-file mr-2"></i>Page
+                        </a>
                     </li>
                     <li className="menu-item p-2">
-                        <a href="/role" className="text-black hover:text-blue-700">Role</a>
+                        <a href="/role" className="text-black hover:text-blue-700">
+                            <i className="pi pi-users mr-2"></i>Role
+                        </a>
                     </li>
                 </ul>
             </div>
